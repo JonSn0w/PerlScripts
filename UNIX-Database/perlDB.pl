ba305398ce64c1d0c71b3c155da5fb0b0bfe7b07
Brett <JonSn0w@users.noreply.github.com>
2016-07-15 16:04:19 -0700

    Fixed issue of searching keys with shared values

--- UNIX-Database/perlDB.pl
+++ UNIX-Database/perlDB.pl
@@ -1,60 +1,58 @@
 #!/usr/bin/perl
-# use strict;
-# use warnings;
+use strict;
+use warnings;
 use DB_File;
-
-#TODO: needs to be able to handle a(n)
 
 my %hash;
 my $data = "memory.db";
+# Associate hash with unix database 
 dbmopen(%hash, $data, 0666) or die "Can't open $data$!\n";
 # %hash=();    # clears hash
 while(<>) {
 	chomp;
-	#discern Rules from Queries
+	#Discern Rules from Queries
 	if(m/what/si && m/\?/s) {     # Query (type-A)
 		$_ =~ s/\?//gsi;
-		my $key = (split / /, $_)[-1];   # grab last word in string
-		$key = lc($key);
+		my $key = lc((split / /, $_)[-1]);    # grab last word in string
 		$key = substr($key, 0, length($key)-1);
 		if(exists($hash{$key})) {
-			my $val = ucfirst($hash{$key});
-			print '[A]: '.ucfirst($key).' is a(n) '.$val."\n";
+			my $val = $hash{$key};
+			my $data = ucfirst((split / /, $val)[-1]); 
+			my $first = (split / /, $_)[0];
+			my $last = (split / /, $_)[-1]; 
+			$val =~ s/$data/$data/si;
+			$key = ucfirst($key);
+			$_ =~ s/$last/$val/i;
+			$_ =~ s/$first/$key/i;
 		} else {
-			print '[A]: '."I don't know $key\n";
+			$key = ucfirst($key);
+			$_ =~ s/what is/I don\'t know/i;
+			$_=~ s/$key/$key/si;
 		}
+		print '[A]: '.$_."\n";
 	} elsif(m/who/si && m/\?/s) {     # Query (type-B)
-		$_ =~ s/\?//gsi;
-		my $val = (split / /, $_)[-1];   # grab last word in string
-		$val = lc($val);
+		$_ =~ s/\?/\./gsi;
+		my $val = lc((split / is /, $_)[1]); 
 		my %rev = reverse %hash;
 		if(exists($rev{$val})) {
 			#TODO: needs to be able to handle multiple
-			my $key = $rev{$val};
-			$val = ucfirst($val);
-			print '[B]: '.ucfirst($key).' is a(n) '.$val."\n";
+			for my $key(sort keys %hash) {
+				if($val =~ m/$hash{$key}/si) {
+					my $data = ucfirst((split / /, $val)[-1]);
+					$val =~ s/$data/$data/si;
+					print '[B]: '.ucfirst($key).' is '.$val."\n";
+				}
+			}
 		} else {
-			print '[B]: '."Nobody is a(n) $val"."\n";
+			my $data = ucfirst((split / /, $val)[-1]); 
+			$val =~ s/$data/$data/si;
+			print '[B]: Nobody is '.$val."\n";
 		}
 	} else {    # Rule
-		$_ =~ s/\.//gsi;
-		my ($key, $val) = split / is a /, $_, 2;
+		my ($key, $val) = (split / is /, $_, 2);
 		$key = lc($key);
 		$hash{$key} = lc($val);
-		$val = ucfirst($val);
-		print ucfirst($key).' is a '.$val."\n";
+		#print ucfirst($key).' is '.$val."\n";
 	}
-}
-# print "\n";
-# for my $key(sort keys %hash) {
-# 	my $val = ucfirst($hash{$key});
-# 	print ucfirst($key).' is a '.$val."\n";
-# }
-print "\n".'%rev:'."\n";
-my %rev = reverse %hash;
-for my $key(sort keys %rev) {
-	my $val = ucfirst($rev{$key});
-	$key = ucfirst($key);
-	print $val.' is a '.$key."\n";
 }
 dbmclose(%hash);
